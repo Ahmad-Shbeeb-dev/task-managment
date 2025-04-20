38d1fc2371e4d72976f71fca0fbdb5f151afe51b
Ahmad-Shbeeb-dev <ahmadshbeeb@windowslive.com>
2025-04-20 21:57:14 +0300

    cleanup

--- apps/nextjs/src/app/dashboard/employees/(employee-form)/add-employee/page.tsx
+++
@@ -1,7 +0,0 @@
-import { AddEmployeeForm } from "~/components/forms/employee-form/AddEmployeeForm";
-
-const AddAnnouncementPage = () => {
-  return <AddEmployeeForm />;
-};
-
-export default AddAnnouncementPage;
--- apps/nextjs/src/app/dashboard/employees/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { LoadingSpinner } from "~/components/LoadingSpinner";
-
-//loading state for the whole page
-export default function Loading() {
-  return <LoadingSpinner />;
-}
